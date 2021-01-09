6d38f71909970a0f3b468b0507c21482c46b2df9
avinashbiradar <avinashbiradar179@gmail.com>
2021-01-09 16:18:46 +0530

    Added:calculate win and loose amount per day for 2 days

--- utility.js
+++ utility.js
@@ -1,11 +1,16 @@
-let STAKE=100;
+
 const BET=1;
 console.log("Welcome to gambling simulation");
 class Utility {
     
-    simulateGambling=()=>{       
-         let winStake=100;
-         let lostStake=100;
+    simulateGambling=()=>{ 
+        let stake=100;      
+         let day = 1;
+         let winAmount;
+         let lostAmount;
+         while(day<=20){
+            let  winStake = 100;
+            let  lostStake = 100;
         while(winStake<150 || lostStake>50){
         let random=Math.floor(Math.random() * 2);
         if (random==BET) {
@@ -19,19 +24,13 @@ class Utility {
              console.log(lostStake);  
                       
         }
-     } 
-     if(winStake==150){
-        console.log("Gambler win 50% of stake");
-        }
-        else{
-            console.log("Gambler loose 50% of stake");
-        }
     }
-    
+     winAmount=winStake-stake;
+     lostAmount=stake-lostStake;
+     console.log("Win amount of day"+day+" is :"+winAmount+" and Lost amount of day"+day+" is :"+lostAmount);
+     day=day+1;
+    }
+  }
 }
-
-
-
-
 
 module.exports = new Utility();
