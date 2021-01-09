a933e87b7e79f6d4d737db9cb8c5f3b8f46acf3f
avinashbiradar <avinashbiradar179@gmail.com>
2021-01-09 14:57:20 +0530

    Added:code if gambler win or loose 50% of stake then resign for the day

--- utility.js
+++ utility.js
@@ -1,19 +1,28 @@
-const STAKE=100;
+let STAKE=100;
 const BET=1;
 console.log("Welcome to gambling simulation");
 class Utility {
-    simulateGambling=()=>{
+    
+    simulateGambling=()=>{       
+         let winStake=100;
+         let lostStake=100;
+ 
         let random=Math.floor(Math.random() * 2);
         if (random==BET) {
             console.log("Gambler wins");
+            winStake=winStake+BET;
+            STAKE = STAKE+(STAKE/2);
+            console.log(STAKE);
         } else {
             console.log("Gambler Looses");
+             lostStake=lostStake-BET;  
+             STAKE = STAKE/2;
+             console.log(STAKE);          
         }
-
     }
     
-    
 }
+
 
 
 
