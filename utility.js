33eea26eafae6cef22a3c9efc8ec8701f423714d
avinashbiradar <avinashbiradar179@gmail.com>
2021-01-09 17:03:39 +0530

    Added:If the gambler wins he will continue next month

--- utility.js
+++ utility.js
@@ -1,30 +1,31 @@
 console.log("Welcome to gambling simulation");
 class Utility {
     simulateGambling=()=>{ 
-         let bet=1;
-         let stake=100;      
-         let day = 1;
-         let winAmount;
-         let lostAmount;
-         let array = [];
-         array.length = 30;
-         var dailyWin = 0;
-         while(day<=30){
-            let  winStake = 100;
-            let  lostStake = 100;
-        while(winStake<150 || lostStake>50){
-        let random=Math.floor(Math.random() * 2);
-        if (random==bet) {
-            lostStake = lostStake-1;
-            dailyWin = dailyWin - 1;
-            array[day-1]=dailyWin;
-           
-        } else {
-            winStake = winStake+1;
-            dailyWin = dailyWin+1;
-            array[day-1]=dailyWin; 
+        let winAmount;
+        let lostAmount;
+     do{
+                let bet=1;
+                let stake=100;      
+                let day = 1;
+                let array = [];
+                array.length = 30;
+                var dailyWin = 0;
+      while(day<=30){
+             let  winStake = 100;
+             let  lostStake = 100;
+     while(winStake<150 || lostStake>50){
+            let random=Math.floor(Math.random() * 2);
+            if (random==bet) {
+                lostStake = lostStake-1;
+                dailyWin = dailyWin - 1;
+                array[day-1]=dailyWin;
+            
+            } else {
+                winStake = winStake+1;
+                dailyWin = dailyWin+1;
+                array[day-1]=dailyWin; 
+            }
         }
-    }
      winAmount=winStake-stake;
      lostAmount=stake-lostStake;
      console.log("Win amount of day"+day+" is :"+winAmount+" and Lost amount of day"+day+" is :"+lostAmount);
@@ -37,11 +38,11 @@ class Utility {
      day=day+1;
     }
   
-  var luckyDay = 0;
-  var unluckyDay = 0;
-  var high = array[0];
-  var low = array[0];
-  for (var i = 1; i < array.length; i++)
+  let luckyDay = 0;
+  let unluckyDay = 0;
+  let high = array[0];
+  let  low = array[0];
+  for (let  i = 1; i < array.length; i++)
   {
           if( high < array[i] )
            {
@@ -53,11 +54,13 @@ class Utility {
                   low = array[i];
                   unluckyDay = i+1;
           }
-
-      }
+     }
       console.log("Luckiest day is day : "+luckyDay+" and He won : "+high);
       console.log("Unluckiest day is day : "+unluckyDay+" and He lost : "+low);
-  }
+      }  while (winAmount>lostAmount);
+    
+    }
+
 }
 
 module.exports = new Utility();
