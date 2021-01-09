8dba6fb0b7bb471f03f922170c33ba7dd865b71f
avinashbiradar <avinashbiradar179@gmail.com>
2021-01-09 16:36:04 +0530

    Added:finding the luckiest and unluckiest day of month

--- utility.js
+++ utility.js
@@ -6,20 +6,23 @@ class Utility {
          let day = 1;
          let winAmount;
          let lostAmount;
+         let array = [];
+         array.length = 30;
+         var dailyWin = 0;
          while(day<=30){
             let  winStake = 100;
             let  lostStake = 100;
         while(winStake<150 || lostStake>50){
         let random=Math.floor(Math.random() * 2);
         if (random==bet) {
-            console.log("Gambler wins");
-            winStake=winStake+bet;
-            console.log(winStake);
-            
+            lostStake = lostStake-1;
+            dailyWin = dailyWin - 1;
+            array[day-1]=dailyWin;
+           
         } else {
-            console.log("Gambler Looses");
-             lostStake=lostStake-bet;
-             console.log(lostStake);  
+            winStake = winStake+1;
+            dailyWin = dailyWin+1;
+            array[day-1]=dailyWin; 
         }
     }
      winAmount=winStake-stake;
@@ -33,6 +36,27 @@ class Utility {
         }
      day=day+1;
     }
+  
+  var luckyDay = 0;
+  var unluckyDay = 0;
+  var high = array[0];
+  var low = array[0];
+  for (var i = 1; i < array.length; i++)
+  {
+          if( high < array[i] )
+           {
+                  high = array[i];
+                  luckyDay = i+1;
+          }
+          if( low > array[i] )
+          {
+                  low = array[i];
+                  unluckyDay = i+1;
+          }
+
+      }
+      console.log("Luckiest day is day : "+luckyDay+" and He won : "+high);
+      console.log("Unluckiest day is day : "+unluckyDay+" and He lost : "+low);
   }
 }
 
